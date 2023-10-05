ae810e330c218012e35552597c648fea580f17b3
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2023-10-05 12:35:52 -0400

    Fixed comment spacing

--- sum02.s
+++ sum02.s
@@ -3,7 +3,7 @@
 .global _start
 _start:
     mov r0, #6     @ Place #number into r0
-    mov r1, #8    @ Place #number into r1
+    mov r1, #8     @ Place #number into r1
     add r0, r0, r1 @ Add r0 and r1 and place sum into r0
     mov r7, #1
     svc 0
